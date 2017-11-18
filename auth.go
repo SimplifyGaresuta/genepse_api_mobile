621f21a4db6a3c3555a9fd20edcaf3d0322dd54e
ryonakao <rodriguez.nak@gmail.com>
2017-11-19 00:23:34 +0900

    権限確認関数の削除

--- auth.go
+++ auth.go
@@ -9,32 +9,6 @@ import (
 	"github.com/stretchr/objx"
 )
 
-type authHandler struct {
-	next http.Handler
-}
-
-func (h *authHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	_, err := r.Cookie("auth")
-	if err == http.ErrNoCookie {
-		// not authenticated
-		w.Header().Set("Location", "/login")
-		w.WriteHeader(http.StatusTemporaryRedirect)
-		return
-	}
-	if err != nil {
-		// some other error
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	// success - call the next handler
-	h.next.ServeHTTP(w, r)
-}
-
-// MustAuth adapts handler to ensure authentication has occurred.
-func MustAuth(handler http.Handler) http.Handler {
-	return &authHandler{next: handler}
-}
-
 // loginHandler handles the third-party login process.
 func loginHandler(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	action := ps.ByName("action")
