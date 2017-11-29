d826f9317600d061955966412ff14673937edf76
ryonakao <rodriguez.nak@gmail.com>
2017-11-29 11:52:03 +0900

    設定ファイルをyamlに

--- src/infra/orm/connect.go
+++ src/infra/orm/connect.go
@@ -5,11 +5,13 @@ import (
 
 	"github.com/jinzhu/gorm"
 	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/spf13/viper"
 )
 
 var db *gorm.DB
 
 func OpenMysql() (err error) {
+	viper.SetConfigName("config")
 	db, err = gorm.Open("mysql", config.UserName+":"+config.Pass+"@/"+config.DbName+"?charset="+config.Charset+"&parseTime="+config.ParseTime+"&loc="+config.Loc)
 	return
 }
