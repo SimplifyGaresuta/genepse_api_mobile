ae60bb231979ed6ef278f2fa2afec6a3d94a00e2
ryonakao <rodriguez.nak@gmail.com>
2017-12-14 16:37:56 +0900

    全エンドポイントに異常系実装

--- app/handler.go
+++ app/handler.go
@@ -20,6 +20,10 @@ import (
 
 type Response interface{}
 
+type exception struct {
+	Message string `json:"message"`
+}
+
 func returnJSON(w http.ResponseWriter, res Response) error {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	// TODO 本番はjson.Marshalを使用する
@@ -34,31 +38,32 @@ func returnJSON(w http.ResponseWriter, res Response) error {
 func userList(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 	query := r.URL.Query()
 	if len(query["limit"]) < 1 {
-		log.Println("limitを指定して下さい。")
-		// TODO 異常系json
+		w.WriteHeader(400)
+		returnJSON(w, exception{Message: "limitを指定して下さい。"})
 		return
 	}
 	limit, err := strconv.Atoi(query["limit"][0])
 	if err != nil {
-		log.Println("クエリパラメータが不正です", err)
-		// TODO 異常系のjson
+		w.WriteHeader(400)
+		returnJSON(w, exception{Message: "limitの値が不正です。"})
 		return
 	}
 	if len(query["offset"]) < 1 {
-		log.Println("offsetを指定して下さい。")
-		// TODO 異常系json
+		w.WriteHeader(400)
+		returnJSON(w, exception{Message: "offsetを指定して下さい。"})
 		return
 	}
 	offset, err := strconv.Atoi(query["offset"][0])
 	if err != nil {
-		log.Println("クエリパラメータが不正です", err)
-		// TODO 異常系のjson
+		w.WriteHeader(400)
+		returnJSON(w, exception{Message: "offsetの値が不正です。"})
 		return
 	}
 	response, err := feed.GetResponse(limit, offset)
 	if err != nil {
 		log.Println("フィード取得時にエラー", err)
-		// TODO 異常系のjson返す
+		w.WriteHeader(500)
+		returnJSON(w, exception{Message: "フィード取得時にエラー。しばらくお待ち下さい。"})
 		return
 	}
 	returnJSON(w, response)
@@ -67,14 +72,15 @@ func userList(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 func userDetail(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	id, err := strconv.Atoi(ps.ByName("id"))
 	if err != nil {
-		log.Println("idが不正です。")
-		// TODO 異常系json
+		w.WriteHeader(400)
+		returnJSON(w, exception{Message: "idが不正です。"})
 		return
 	}
 	user, err := detail.GetUser(id)
 	if err != nil {
 		log.Println("プロフィール取得時にエラー", err)
-		// TODO 異常系json
+		w.WriteHeader(500)
+		returnJSON(w, exception{Message: "プロフィール取得時にエラー。しばらくお待ち下さい。"})
 		return
 	}
 	returnJSON(w, user)
@@ -84,13 +90,14 @@ func userUpdate(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	defer r.Body.Close()
 	id, err := strconv.Atoi(ps.ByName("id"))
 	if err != nil {
-		log.Println("idが不正です。")
-		// TODO 異常系json
+		w.WriteHeader(400)
+		returnJSON(w, exception{Message: "idが不正です。"})
 		return
 	}
 	if err := detail.UpdateUser(uint(id), r.Body); err != nil {
 		log.Println("プロフィール更新時にエラー", err)
-		// TODO 異常系json
+		w.WriteHeader(500)
+		returnJSON(w, exception{Message: "プロフィール更新時にエラー。しばらくお待ち下さい。"})
 		return
 	}
 }
@@ -103,12 +110,14 @@ func productCreate(w http.ResponseWriter, r *http.Request, ps httprouter.Params)
 	}
 	if err := r.ParseForm(); err != nil {
 		log.Println("リクエストbodyが不正です。", err)
-		// TODO 異常系json
+		w.WriteHeader(400)
+		returnJSON(w, exception{Message: "リクエストボディが不正です。"})
 		return
 	}
 	userID, err := strconv.Atoi(strings.Join(r.Form["user_id"], ""))
 	if err != nil {
-		// TODO 異常系json
+		w.WriteHeader(400)
+		returnJSON(w, exception{Message: "user_idの値が不正です。"})
 		return
 	}
 	operator := &detail.ProductOperator{
@@ -121,7 +130,8 @@ func productCreate(w http.ResponseWriter, r *http.Request, ps httprouter.Params)
 	response, err := detail.CreateProduct(operator)
 	if err != nil {
 		log.Println("作品登録時にエラー", err)
-		// TODO 異常系json
+		w.WriteHeader(500)
+		returnJSON(w, exception{Message: "作品登録時にエラー。しばらくお待ち下さい。"})
 		return
 	}
 	returnJSON(w, response)
@@ -135,14 +145,14 @@ func productUpdate(w http.ResponseWriter, r *http.Request, ps httprouter.Params)
 
 	id, err := strconv.Atoi(ps.ByName("id"))
 	if err != nil {
-		log.Println("idが不正です。")
-		// TODO 異常系json
+		w.WriteHeader(400)
+		returnJSON(w, exception{Message: "user_idの値が不正です。"})
 		return
 	}
 
 	if err := r.ParseForm(); err != nil {
-		log.Println("リクエストbodyが不正です。", err)
-		// TODO 異常系json
+		w.WriteHeader(400)
+		returnJSON(w, exception{Message: "リクエストボディが不正です。"})
 		return
 	}
 	operator := &detail.ProductOperator{
@@ -154,7 +164,8 @@ func productUpdate(w http.ResponseWriter, r *http.Request, ps httprouter.Params)
 	}
 	if err := detail.UpdateProduct(operator); err != nil {
 		log.Println("作品更新時にエラー", err)
-		// TODO 異常系json
+		w.WriteHeader(500)
+		returnJSON(w, exception{Message: "作品更新時にエラー。しばらくお待ち下さい。"})
 		return
 	}
 }
@@ -164,7 +175,8 @@ func locationUpdate(w http.ResponseWriter, r *http.Request, ps httprouter.Params
 	id := ps.ByName("id")
 	if err := location.UpdateLocation(id, r.Body); err != nil {
 		log.Println("位置情報更新時にエラー", err)
-		// TODO 異常系json
+		w.WriteHeader(500)
+		returnJSON(w, exception{Message: "位置情報更新時にエラー。しばらくお待ち下さい。"})
 		return
 	}
 }
@@ -172,15 +184,16 @@ func locationUpdate(w http.ResponseWriter, r *http.Request, ps httprouter.Params
 func nearUsers(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	query := r.URL.Query()
 	if len(query["user_id"]) < 1 {
-		log.Println("user_idを指定して下さい。")
-		// TODO 異常系json
+		w.WriteHeader(400)
+		returnJSON(w, exception{Message: "user_idを指定して下さい。"})
 		return
 	}
 	userID := query["user_id"][0]
 	res, err := location.GetNearUsers(userID, 100)
 	if err != nil {
 		log.Println("近くのユーザー検索時にエラー。", err)
-		// TODO 異常系json
+		w.WriteHeader(500)
+		returnJSON(w, exception{Message: "近くのユーザー検索時にエラー。しばらくお待ち下さい。"})
 		return
 	}
 	returnJSON(w, res)
@@ -214,7 +227,8 @@ func callback(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	user, err := middleware.GetUser(providerName, r.URL.RawQuery)
 	if err != nil {
 		log.Println(err)
-		// TODO 異常系json
+		w.WriteHeader(500)
+		returnJSON(w, exception{Message: "ユーザー取得時エラー。しばらくお待ち下さい。"})
 		return
 	}
 	accountID := user.IDForProvider(providerName)
@@ -229,14 +243,16 @@ func callback(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 		userID, err = registration.UserID(provider, accountID)
 		if err != nil {
 			log.Println(err)
-			// TODO 異常系json
+			w.WriteHeader(500)
+			returnJSON(w, exception{Message: fmt.Sprintf("%sのアカウントid取得時にエラー", providerName)})
 			return
 		}
 	} else { // 登録
 		userID, err = registration.Register(user.Name(), user.AvatarURL(), accountID, providerName, r.Context())
 		if err != nil {
 			log.Println(err)
-			// TODO 異常系json
+			w.WriteHeader(500)
+			returnJSON(w, exception{Message: "ユーザー登録時にエラー。しばらくお待ち下さい。"})
 			return
 		}
 	}
@@ -247,5 +263,5 @@ func callback(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 }
 
 func healthCheck(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	log.Println("I am healthy!")
+	fmt.Println("I am healthy!")
 }
